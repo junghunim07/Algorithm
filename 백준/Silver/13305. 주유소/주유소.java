e546d053cb068edae5e4d9b3e41fee6a3043128b
JungHunLim <137620768+junghunim07@users.noreply.github.com>
2024-11-23 15:54:42 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 34404 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] distance;
+    static int[] gasStation;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        distance = new int[n - 1];
+        gasStation = new int[n];
+
+        String[] str = br.readLine().split(" ");
+        for (int i = 0; i < n - 1; i++) {
+            distance[i] = Integer.parseInt(str[i]);
+        }
+
+        str = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            gasStation[i] = Integer.parseInt(str[i]);
+        }
+
+        System.out.println(solve());
+    }
+
+    static int solve() {
+        int pay = 0;
+        int minCost = gasStation[0];
+
+        for (int i = 0; i < distance.length; i++) {
+            if (gasStation[i] < minCost) {
+                minCost = gasStation[i];
+            }
+
+            pay += distance[i] * minCost;
+        }
+
+        return pay;
+    }
+}
