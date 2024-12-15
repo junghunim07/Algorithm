191e09276a8d10fe9474ca389367461d41e2fb6d
JungHunLim <137620768+junghunim07@users.noreply.github.com>
2024-12-15 22:27:41 +0900

    [Silver I] Title: 팔, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String L = st.nextToken();
+        String K = st.nextToken();
+
+        int answer = 0;
+
+        if (L.length() != K.length()) {
+            System.out.println(answer);
+        } else {
+            int i = 0;
+            while (i < L.length() && L.charAt(i) == K.charAt(i)) {
+                if (L.charAt(i) == '8') {
+                    answer++;
+                }
+                i++;
+            }
+            System.out.println(answer);
+        }
+    }
+}
