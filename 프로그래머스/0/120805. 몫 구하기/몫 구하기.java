a9e7150417f8a64da196e643cbd008d9de46da7e
JungHunLim <137620768+junghunim07@users.noreply.github.com>
2024-12-23 17:23:57 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -1,6 +1,15 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 / num2;
+        int answer = 0;
+        
+        if (checkInRange(num1) && checkInRange(num2)) {
+            answer = num1 / num2;
+        }
+        
         return answer;
+    }
+    
+    boolean checkInRange(int target) {
+        return target > 0 && target <= 100;
     }
 }
