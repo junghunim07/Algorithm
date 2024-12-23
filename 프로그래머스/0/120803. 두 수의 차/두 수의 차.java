d86ed88e4d62bd17f098ceef02f7cabea972c94a
JungHunLim <137620768+junghunim07@users.noreply.github.com>
2024-12-23 17:17:28 +0900

    [level 0] Title: 두 수의 차, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -1,6 +1,15 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 - num2;
+        int answer = 0;
+        
+        if (validInRange(num1) && validInRange(num2)) {
+            answer = num1 - num2;
+        }
+        
         return answer;
+    }
+    
+    boolean validInRange(int target) {
+        return target >= -50_000 && target <= 50_000;
     }
 }
