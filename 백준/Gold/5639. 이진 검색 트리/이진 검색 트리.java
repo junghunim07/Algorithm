199c3d09614000969332854cc8182878f1ab138e
JungHunLim <137620768+junghunim07@users.noreply.github.com>
2025-01-04 15:37:34 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 596 ms, Memory: 29032 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Main {
+
+    static class Node {
+        int value;
+        Node left, right;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int value) {
+            if (value < this.value) {
+                if (this.left == null) {
+                    this.left = new Node(value);
+                } else {
+                    this.left.insert(value);
+                }
+            } else {
+                if (this.right == null) {
+                    this.right = new Node(value);
+                } else {
+                    this.right.insert(value);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+
+        while (true) {
+            String input = br.readLine();
+            if (input == null || input.isEmpty()) {
+                break;
+            }
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+
+    static void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value);
+    }
+}
